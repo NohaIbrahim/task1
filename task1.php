776fb475bb2346da7d063a4034be02649204631d
noha <noha.ibrahim16@gmail.com>
2023-09-08 18:44:51 +0200

    task2

---
+++ task1.php
@@ -0,0 +1,56 @@
+<?php
+///////////1 mulitble and sum////
+// $s=readline();
+// $d=readline();
+// $f=readline();
+// echo ($s+$d)*$f;
+/////////////////////////2 size of box//////
+// $q=5;
+// $e=10;
+// $r=readline();
+// echo $q*$e*$r;
+/////////////3 hour and second////
+
+// $g=(int)readline();
+// echo 3600*$g;
+///////////4 area tringle/////
+// $b=readline();
+// $y=readline();
+// echo .5*$b*$y;
+////////////////5 day and year//////
+// $h=readline();
+// echo $h*365;
+// ///////////////////////////
+//////////////6////lentgh of string (num of char with spaces)////
+
+
+ $str= "EraaSoft Learn By practice";
+ // echo strlen ($str);
+ /////len without spaces
+ ///////////echo trim($str," ");
+
+//////////8 num of word////
+//echo str_word_count($str)
+///////////9 by found or no ////
+// $o=substr_count($str,'By');
+// echo $o
+///////// reblace witeout by
+// $p= str_replace('By','',$str);
+// echo $p;
+// echo str_replace(' ','',$p)
+// ////////concatentane12//
+ $str3='eraa';
+$str4='soft';
+ $fullname=$str3.$str4;
+// echo $fullname;
+////13 compare///
+echo strcmp($str3,$str4);
+
+////////////14
+// $str2='EraaSoft';
+// print_r( str_split($str2,2))
+//////12 COMPARE //
+
+
+
+?>
