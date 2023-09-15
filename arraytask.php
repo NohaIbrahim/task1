335a54b8ca8f21a869f5f606fc4cf2d501463fa1
noha <noha.ibrahim16@gmail.com>
2023-09-15 18:13:53 +0200

    taskof array

---
+++ arraytask.php
@@ -0,0 +1,185 @@
+<?php
+   ///////1111111111111
+//$e=0;
+
+//for ($i=0;$i<10;$i++){
+ // $n=rand(1,100);
+ // $d[]=$n;
+ // $e+=$n;
+//}
+// print_r($d);
+// echo $e."<br>";
+//echo $e/10;
+
+
+
+///////////2222222222222222
+// $array=[1,2,3,4,5,6,7,8,9,0];
+   //$w=0;
+  // for($i=0;$i<2;$i++){
+    // for($j=0;$j<5;$j++){
+   //    $secondarr[$i][$j]=$array[$w];
+    //   $w++;
+    // }
+ //  }
+   
+ //  print_r($secondarr);
+ ////
+
+
+
+ ///////////////3333333333////
+   
+ //  $x=[1,10,5,2,11];
+ // $min=$x[0];
+//  $maxx=$x[0];
+   
+   //for($i=0;$i<5;$i++){
+   //  if($x[$i]<$min)
+    // $min=$x[$i];
+ //if($x[$i]>$maxx)
+ //$maxx=$x[$i];
+     
+  // }
+ //  echo "large num is $maxx";
+  // echo "small num is $min";
+  /////////////////4444444444444
+  ////////44444444444
+ // $x=[1,10,5,2,11];$g=0;$s=0;
+//  for($i=0;$i<5;$i++){
+  //  if($x[$i]>3)
+//    $g++;
+ //   else if ($x[$i]<3)
+//    $s++;
+//  }
+ // echo "numbers greater than3 is $g";
+ // echo "number smaller than 3 is $s";
+
+
+
+
+ ////////////7777777777777////
+ //function found($name){
+ //  for ($fo=0;$fo<count($name);$fo++)
+  // {
+    // if($name[$fo]=="Bob"){
+      // $t=1;
+       //break;
+     //}
+    // else $t=-1;
+    // }
+     
+       
+     
+   
+   //echo $t;
+  // }
+   //found(["alice","chalice"]);
+    //found(["alice","Bob",chalice"]);
+
+
+   ///////////////666666
+   // $q=[1,7,0,10,66,0,3,45,0,0];
+  // $k=0;
+  // for($i=0;$i<10;$i++){
+    // if ($q [$i]!=0)
+     // $w[]=$q[$i];
+     // else 
+     // $k++;
+   //  }
+  // for($t=0;$t<$k;$t++){
+      // $w[]=0;
+    //}
+ // print_r($w);
+
+
+
+///////////////555555555
+// function small($u){  
+//     for($i=0;$i<count($u);$i++){
+ //     $b=  str_split($u[$i]);  
+//    for($j=0;$j<count ($b);$j++){  
+//   if($j==0)
+//    $m[] =strtoUpper ($b[0]) ;
+//    else$m[]=$b[$j];}
+//    $aa=implode($m);
+//    $f[]=$aa;
+//   $m=[];
+//    }
+//  print_r($f);
+//  print_r($u);
+//   }
+    
+//      small(["eraa","backend","groub313"]);
+  //////////////8888888888//////////
+
+
+
+//////////////8888888888//////////
+   // $x=[10,55,2,3,4,5,6,7,8,9,11];
+   // $maax=$x[0];
+   // for($i=0;$i<11;$i++){
+      
+  //  if($x[$i]>$maax){
+    //  $g=$maax;
+      //$maax=$x[$i];
+    //  }
+     // else if ($x[$i]<$maax&&$x[$i]>$g)
+     // $g=$x[$i];
+  //  }
+  //  echo $g;
+  ///////another way
+  //sort($x);
+  //print_r($x);
+  //echo $x[count($x)-2];
+
+
+  ////////__/////////999999999999
+//   $arr=[11,55,1221,24,43,44,545,6,777,810,94,140];
+  //  function found($arr,$x){
+    //  $x=1221;
+    
+//  $foundnum=in_array($x,$arr);
+ // if($foundnum==1){
+   // echo"found" ;
+  //  if($x>0){
+    //  echo"positive";
+    //  }
+ //   else 
+  //  echo"negative";
+  //  if($x%2==0)
+  //  echo "even";
+    //else echo "odd";
+    //$u=$x;
+  // $o=0;
+  
+  //while($u>1){
+  //  $u=($u/10);
+   // $o++;
+    
+ // }
+//  echo"number of digit". $o;
+//  $w=0;
+ // for ($y=2;$y<$x;$y++){
+//  if($x%$y==0){
+//  $w="prime";
+  //break;}
+  //else $w="not prime ";
+ // }
+ // echo $w;
+  //$g=str_split ($x);// number of digit =count($g)
+ // $l=$o-1;
+ // for($i=0;$i<$o;$i++){
+   // if($g[$i]!=$g[$l]){
+    //  $sol="no";
+     // break;
+   // }
+    //else{$sol="yes";
+   // }
+  //  $l--;
+  //}
+ // echo $sol;
+//  }
+ // else echo "nofound";
+ 
+ ?>
---
+++ searcharray.php
@@ -0,0 +1,42 @@
+<?php
+//function myfunction($v1,$v2)
+//{
+//if ($v1===$v2)
+ // {
+ // return "same";
+ // }
+//return "different";
+//}
+
+//$a1=array("Horse","Dog","Cat");
+//$a2=array("Cow","Dog","Rat");
+//print_r(array_map("myfunction",$a1,$a2));
+//?>
+///////////
+//<?php
+//$a1=array("Dog","Cat");
+//$a2=array("Puppy","Kitten","horse");
+//print_r(array_map(null,$a1,$a2));
+//?>
+<?php
+
+// function meyfunction($v1,$v2)
+// {
+// return $v1*$v2;
+// }
+// $a=array(10,15,20);
+// $array3=array(1,2,3);
+// print_r(array_reduce($a,"meyfunction",5));
+// print_r(array_map("meyfunction",$a,$array3));
+?>
+<?php
+// function test_odd($var)
+//   {
+//   return($var >=5);
+//   //return ($var>5);
+//   }
+
+// $a1=array(1,3,33,5);
+// print_r(array_filter($a1,"test_odd"));
+?>
+
